65465ea9b575755dceef310bffda9188e85489d6
赤琦 <redblue9771@gmail.com>
2019-12-13 21:48:49 +0800

    Update package && Fix 404 err

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,27 +1,23 @@
-import React from 'react'
 import Link from 'gatsby-plugin-transition-link/AniLink'
-import MainLayout from '../common/MainLayout'
-import SEO from '../common/SEO'
+import React from 'react'
 import Carousel from '../common/Carousel'
 
 export default ({ location }) => {
   return (
-    <MainLayout location={location} title="404 Not Found">
-      <main className="text-center">
-        <img alt="404" src="/img/404.svg" />
-        <div className="alert alert-primary" role="alert">
-          <h4>
-            页面不存在，旧站的页面链接已经改变，请到
-            <Link fade to="/">
-              {' '}
-              主页{' '}
-            </Link>
-            中寻找
-          </h4>
-        </div>
-        <h4>来看看这些吧</h4>
-        <Carousel />
-      </main>
-    </MainLayout>
+    <main className="text-center">
+      <img alt="404" src="/img/404.svg" />
+      <div className="alert alert-primary" role="alert">
+        <h4>
+          页面不存在，旧站的页面链接已经改变，请到
+          <Link fade to="/">
+            {' '}
+            主页{' '}
+          </Link>
+          中寻找
+        </h4>
+      </div>
+      <h4>来看看这些吧</h4>
+      <Carousel />
+    </main>
   )
 }
